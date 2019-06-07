b05788dbf72cd6387fcd3d418e799a3c4a206239
javier Chacón <javierchacon262@gmail.com>
2019-06-06 20:45:57 -0500

    Fixed plotting and saving the best crom per generation and the fab asignation for that best crom.

--- run.m
+++ run.m
@@ -11,16 +11,26 @@ load('N100P30F4M10_1.mat');
 %Generar poblacion inicial
 inipob = init(P, AST, AT);
 sp = size(inipob, 2);
+vecGen = [];
+vecFab = [];
 for i = 1:iter
     disp(i)
     vecmsp = [];
+    matF = [];
     for j = 1:sp
         [vecF, OG, vecS] = evalPro(inipob(:, j), ASIG, ST, PT);
         vecE = evalEn(inipob(:, j), AST, AT);
         mkspn = objfunc(OG, vecF, vecE, inipob(:, j), vecS);
         vecmsp = [vecmsp, mkspn];
+        matF = [matF; vecF];
     end
     inipob = [inipob; vecmsp];
+    sc = size(inipob, 1);
+    [minv, idx] = sortrows(inipob', sc, 'ascend');
+    minCr = minv(1, :)';
+    minF = matF(idx(1), :);
+    vecGen = [vecGen, minCr];
+    vecFab = [vecFab; minF];
     selected = selection(inipob);
     crossed = crossover(selected, probC);
     mutated = mutation(crossed, probM);
@@ -35,9 +45,22 @@ for j = 1:sp
     vecmsp = [vecmsp, mkspn];
 end
 inipob = [inipob; vecmsp];
+sc = size(inipob, 1);
+[minv, idx] = sortrows(inipob', sc, 'ascend');
+minCr = minv(1, :)';
+minF = matF(idx(1), :);
+vecGen = [vecGen, minCr];
+vecFab = [vecFab; minF];
 xr = 1:sp;
+xr2 = 1:iter+1;
 figure
+subplot(1, 2, 1)
 scatter(xr, vecmsp)
 title('Resultados')
 xlabel('cromosomas')
 ylabel('makespan')
+subplot(1, 2, 2)
+scatter(xr2, vecGen(end, :))
+title('Mejor Resultado por Generacion')
+xlabel('Generaciones')
+ylabel('Mejor MakeSpan')
